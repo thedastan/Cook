69d1d8d286fc93289cbd0e5802db587ead27dcd0
thedastan <dastanshermatov@gmail.com>
2021-05-20 16:50:12 +0600

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow:3,
+        autoplay:true,
+        autoplaySpeed: 1000
+    })
+})
+
+
+new WOW().init();
